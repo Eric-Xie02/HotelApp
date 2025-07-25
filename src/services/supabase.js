3a8e5edc41c7d54d3edd6f2298e7bac1b8083ab7
Eric Xie <exie102202@gmail.com>
2025-07-25 12:18:17 -0400

    Added cabin and settings functions

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_KEY;
 
-const supabaseUrl = "https://nwynmtfkwgudhpmwzrvc.supabase.co";
+export const supabaseUrl = "https://nwynmtfkwgudhpmwzrvc.supabase.co";
 const supabase = createClient(supabaseUrl, SUPABASE_KEY);
 
 export default supabase;
